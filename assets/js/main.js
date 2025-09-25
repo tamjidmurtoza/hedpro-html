91a627c6f5fb8e1dc13ec7003e12c9cc7162f0e3
Fatiha Zannat <fatihazannat.n@gmail.com>
2025-09-25 09:48:18 +0600

    home 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,7 @@
   | 13. Dynamic contact form
   | 14. AOS Animation
   | 15. hobbleEffect
+  | 16. beforeAfterSlide
   */
 
   /*--------------------------------------------------------------
@@ -63,6 +64,7 @@
     smoothScroll();
     aosInit();
     hobbleEffect()
+    beforeAfterSlider()
     $(".tom_select").each(function () {
       new TomSelect(this, {
         create: false,
@@ -506,6 +508,88 @@
         $(this).find(".cs_hover_layer_1").removeAttr("style");
         $(this).find(".cs_hover_layer_2").removeAttr("style");
       });
+  }
+
+  /*--------------------------------------------------------------
+   14. Before After Slider
+ --------------------------------------------------------------*/
+  function beforeAfterSlider() {
+    if ($.exists('.cs_before_after')) {
+      var supportsTouch =
+        'ontouchstart' in window || navigator.msMaxTouchPoints;
+      $('.cs_before_after').each(function () {
+        var $container = $(this),
+          $before = $container.find('.cs_before'),
+          $after = $container.find('.cs_after'),
+          $handle = $container.find('.cs_handle_before_after');
+
+        var maxX = $container.outerWidth(),
+          offsetX = $container.offset().left,
+          startX = 0;
+
+        var touchstart, touchmove, touchend;
+        var mousemove = function (e) {
+          e.preventDefault();
+          var curX = e.clientX - offsetX,
+            diff = startX - curX,
+            curPos = (curX / maxX) * 100;
+          if (curPos > 100) {
+            curPos = 100;
+          }
+          if (curPos < 0) {
+            curPos = 0;
+          }
+          $before.css({ right: 100 - curPos + '%' });
+          $handle.css({ left: curPos + '%' });
+        };
+        var mouseup = function (e) {
+          e.preventDefault();
+          if (supportsTouch) {
+            $(document).off('touchmove', touchmove);
+            $(document).off('touchend', touchend);
+          } else {
+            $(document).off('mousemove', mousemove);
+            $(document).off('mouseup', mouseup);
+          }
+        };
+        var mousedown = function (e) {
+          e.preventDefault();
+          startX = e.clientX - offsetX;
+          if (supportsTouch) {
+            $(document).on('touchmove', touchmove);
+            $(document).on('touchend', touchend);
+          } else {
+            $(document).on('mousemove', mousemove);
+            $(document).on('mouseup', mouseup);
+          }
+        };
+
+        touchstart = function (e) {
+          console.log(e);
+          mousedown({
+            preventDefault: e.preventDefault,
+            clientX: e.originalEvent.changedTouches[0].pageX,
+          });
+        };
+        touchmove = function (e) {
+          mousemove({
+            preventDefault: e.preventDefault,
+            clientX: e.originalEvent.changedTouches[0].pageX,
+          });
+        };
+        touchend = function (e) {
+          mouseup({
+            preventDefault: e.preventDefault,
+            clientX: e.originalEvent.changedTouches[0].pageX,
+          });
+        };
+        if (supportsTouch) {
+          $handle.on('touchstart', touchstart);
+        } else {
+          $handle.on('mousedown', mousedown);
+        }
+      });
+    }
   }
 
 
